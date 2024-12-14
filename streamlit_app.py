791be2a063f7f2a746a774def6f6186379aa3d42
Rafio107 <rafiotsuka@gmail.com>
2024-12-15 00:46:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image, ImageFilter
 import numpy as np
 
-# Fungsi untuk denoising menggunakan filter median
 def denoise_image_pillow(image):
     """
     Applies Median Filter for Denoising using Pillow.
@@ -15,19 +14,13 @@ def denoise_image_pillow(image):
     """
     return image.filter(ImageFilter.MedianFilter(size=3))
 
-# Halaman utama yang menampilkan anggota kelompok
 def main_page():
     st.title("Linear Algebra Group 2 Class 2 [2023]")
 
     col1, col2 = st.columns(2)
 
     with col1:
-        # Meminta pengguna mengunggah gambar logo jika dijalankan di cloud
-        uploaded_logo = st.file_uploader("Upload University Logo", type=["png", "jpg", "jpeg"])
-        if uploaded_logo is not None:
-            st.image(uploaded_logo, caption="University Logo", use_column_width=True)
-        else:
-            st.write("Please upload the university logo.")
+        st.image("c:/MainStorageVault/Documents/Project/ImageRestore_Project/PresidentUniversityLogo.png")
 
     with col2:
         st.write("**Group Members:**")
@@ -36,7 +29,6 @@ def main_page():
         st.write("- Muhammad Isfan Nabil Hanif")
         st.write("- Pambudi Setyo Wicaksono")
 
-# Halaman untuk denoising gambar
 def denoising_page():
     st.title("Image Denoising with Pillow")
 
@@ -52,19 +44,16 @@ def denoising_page():
         # Tampilkan gambar asli dan yang sudah di-denoise
         st.image([image, denoised_image], caption=['Original Image', 'Denoised Image'], use_container_width=True)
 
-# Halaman tentang image denoising
 def about_page():
     st.title("About Image Transformation: Denoiser")
 
     col1, col2 = st.columns(2)
 
     with col1:
-        # Meminta pengguna mengunggah gambar denoiser effect jika dijalankan di cloud
-        uploaded_image = st.file_uploader("Upload Denoiser Effect Image", type=["png", "jpg", "jpeg"], key="denoiser")
-        if uploaded_image is not None:
-            st.image(uploaded_image, caption="Denoiser Effect", use_column_width=True)
-        else:
-            st.write("Please upload the denoiser effect image.")
+        # Menggunakan gambar yang diunggah sebagai gambar statis
+        image_path = "/mnt/data/image.png"  # Lokasi gambar yang kamu unggah
+        image = Image.open(image_path)
+        st.image(image, caption="Denoiser Effect", use_column_width=True)
 
     with col2:
         st.write("**Image Denoising**")
